ca6782bfd8402dede8ed384d73f0bde249962fad
josh <joshrandall8478@gmail.com>
2023-02-27 22:21:46 -0500

    fixed JS

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
 
 function showProject(project_id, preview_id){
-    var projectPreview = document.querySelector('.project__preview');
-    var projectItem = document.querySelector('.project__item');
+    var projectPreview = document.querySelector('.project__preview.active');
+    var projectItem = document.querySelector('.project__item.active');
     var proj_id = document.querySelector(project_id);
     var prev_id = document.querySelector(preview_id);
     projectPreview.classList.remove('active');
