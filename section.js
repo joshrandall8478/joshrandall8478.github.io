90bd2e609ad08a82b092cdda4c079daec1b288fa
joshrandall8478 <joshrandall8478@gmail.com>
2023-04-05 01:04:35 -0400

    added comments from copilot

--- section.js
+++ section.js
@@ -2,17 +2,20 @@
 
 
 document.addEventListener('click', (element)=>{
+    // if the click is not on the dropdown button or inside the dropdown, close all dropdowns
     const isDropDownBtn = element.target.matches("[data-dropdown-button]")
     if (!isDropDownBtn && element.target.closest('[data-dropdown]') != null){
         return
     }
     
+    // if the click is on the dropdown button, toggle the dropdown
     let currentDropdown;
     if (isDropDownBtn){
         currentDropdown = element.target.closest('[data-dropdown]')
         currentDropdown.classList.toggle('active')
     }
 
+    // close all other dropdowns
     document.querySelectorAll('[data-dropdown].active').forEach((dropdown => {
         if(dropdown == currentDropdown) return
         dropdown.classList.remove('active')
