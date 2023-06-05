a50a4ebba0cffa376bef72222a81d5dab8a86f8a
josh <joshrandall8478@gmail.com>
2023-06-05 16:25:56 -0400

    working progress with projects section

---
+++ projects.js
@@ -0,0 +1,30 @@
+// Load projects from json file into data constant
+const data = fetch('./projects.json');
+console.log(data);
+// Load projects from data constant into div
+const projectsList = document.getElementById("projects-list");
+// Loop through projects and add each project to div
+let listHTML = '';
+for (let i = 0; i < data.length; i++) {
+    const project = data[i];
+    console.log(project.name);
+    // add project name as a button to div with id projects-list
+    const button = document.createElement('button');
+    button.innerHTML = project.name;
+    button.classList.add('project-btn');
+    button.setAttribute('onclick', 'showProject(' + i + ')');
+    listHTML += button.outerHTML;
+}
+projectsList.innerHTML = listHTML;
+
+function showProject(index) {
+    // console.log(data[index]);
+    // const project = data[index];
+    // const projectDiv = document.getElementById('project');
+    // projectDiv.innerHTML = `
+    //     <h2>${project.name}</h2>
+    //     <p>${project.description}</p>
+    //     <p>${project.technologies}</p>
+    //     <a href="${project.url}">View Project</a>
+    // `;
+}
