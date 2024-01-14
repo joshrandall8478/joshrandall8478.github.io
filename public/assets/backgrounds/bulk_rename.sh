90d150182ee6017d2688bda82130f4022c644f7b
joshrandall8478 <joshrandall8478@gmail.com>
2024-01-13 19:08:09 -0500

    parallax complete

---
+++ public/assets/backgrounds/bulk_rename.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+ext=".gif"
+
+if [ "$#" -eq 0 ]; then
+	echo "No arguments provided."
+	echo "Usage: ./bulk_rename.sh [/destination] [extension]"
+	exit
+fi
+
+cd $1
+ext="$2"
+
+
+ls -v | cat -n | while read n f; do mv -n -v "$f" "$n.$ext"; done
+
+
