2b0e23f213bc28790f744dfbbbb00e6104c81950
joshrandall8478 <joshrandall8478@gmail.com>
2024-04-20 02:46:52 -0400

    added more backgrounds

--- public/assets/backgrounds/bulk_rename.sh
+++ public/assets/backgrounds/bulk_rename.sh
@@ -6,10 +6,17 @@ if [ "$#" -eq 0 ]; then
 	exit
 fi
 
+if [ "$1" == "-h" ]; then
+	echo "Usage: ./bulk_rename.sh [/destination] [extension]"
+        exit
+fi
+
 cd $1
 ext="$2"
 
 
 ls -v | cat -n | while read n f; do mv -n -v "$f" "$n.$ext"; done
+
+echo "Backgrounds renamed. Remember to adjust the background limit in the index.astro JS."
 
 
